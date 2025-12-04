8e1a2caa1442fadec601c6ee559897b610bde658
Jagadeesh <jagadeeshreddimasu@gmail.com>
2025-12-04 19:23:31 +0530

    Add project files.

---
+++ WebApplication1/Controllers/StudentsController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public StudentsController (ApplicationDbContext context)
+        {
+            this.context = context; 
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var student = await context.Students.ToListAsync();
+            return View(student);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Students student)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Students.Add(student);
+                await context.SaveChangesAsync();
+                return RedirectToAction("Index", "Students");
+            }
+            return View(student);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Students student)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Update(student);
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Students");
+            }
+
+            return View(student);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            context.Students.Remove(student);
+            await context.SaveChangesAsync();
+            return RedirectToAction("Index", "Students");
+        }
+    }
+} 
---
+++ WebApplication1/Models/students.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email  { get; set; }
+        public string Mobileno { get; set; }
+
+    }
+}
